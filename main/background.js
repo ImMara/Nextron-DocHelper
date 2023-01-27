cf7914a098c2aa4db0fbc7b2ae238f77eb0e4ffd
ImMara <alaneraerts@live.be>
2023-01-27 13:10:41 +0100

    updater fixed | signed fixed (need to fix identifier apple)

--- main/background.js
+++ main/background.js
@@ -50,6 +50,7 @@ ipcMain.on('restart', (event) => {
     autoUpdater.quitAndInstall(true,true);
 });
 
+
 ipcMain.on('app_version', (event) => {
     event.sender.send('app_version', { version: app.getVersion() });
 });
